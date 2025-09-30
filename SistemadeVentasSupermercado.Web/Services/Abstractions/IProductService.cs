5e75142f30f596afa1dd4ca4b06a21f89222e2e1
ismael-dev-admin <junioribd2006@gmail.com>
2025-09-30 17:49:46 -0500

    CRUD creado

--- SistemadeVentasSupermercado.Web/Services/Abstractions/IProductService.cs
+++ SistemadeVentasSupermercado.Web/Services/Abstractions/IProductService.cs
@@ -6,8 +6,19 @@ namespace SistemadeVentasSupermercado.Web.Services.Abstractions
     public interface IProductService
     {
 
+        // C - Create (Crear)
         public Task<Response<ProductDTO>> CreateAsync(ProductDTO dto);
+
+        // D - Delete (Eliminar)
+        public Task<Response<object>> DeleteAsync(Guid id);
+
+        // U - Update (Actualizar/Editar)
+        public Task<Response<ProductDTO>> EditAsync(ProductDTO dto);
+
+        // R - Read (Leer lista)
         public Task<Response<List<ProductDTO>>> GetListAsync();
 
+        // R - Read (Leer uno)
+        public Task<Response<ProductDTO>> GetOneAsync(Guid id);
     }
 }
